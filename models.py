4622ed9ff4db2c18b103cf54e9a6489a9314aff3
twedcl01 <47513469+twedcl01@users.noreply.github.com>
2019-12-12 01:29:23 -0600

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from config import db, ma
 
 class Play(db.Model):
     __tablename__ = "PLAY"
-    play_num = db.Colum(db.Integer, primary_key=True)
+    play_num = db.Column(db.Integer, primary_key=True)
     play_id = db.Column(db.String)
     title = db.Column(db.String)
     text = db.Column(db.String)
